05823f0a28e9fcc0b8782af5b6bb8a466b19d8b6
mohammedmoueesa-source <mohammedmoueesa@gmail.com>
2026-07-13 12:00:42 +0530

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,13 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyD1MG-I6pgD91SEkKbawdVXsbhnWZ_4HPY",
+    authDomain: "na-coaching-5b922.firebaseapp.com",
+    projectId: "na-coaching-5b922",
+    storageBucket: "na-coaching-5b922.firebasestorage.app",
+    messagingSenderId: "268957725356",
+    appId: "1:268957725356:web:78bf309de4ed7b3b01e509",
+    measurementId: "G-9W61JY0BSZ"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref('nacc')
